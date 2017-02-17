6646f0972fc2daed98e64e5ae0db8e75581dac5d
Eddie S Hsieh <eddie_s_hsieh@trend.com.tw>
2017-02-17 19:08:05 +0800

    remove --exe

--- unit_run.sh
+++ unit_run.sh
@@ -1,1 +1,1 @@
-nosetests --with-xunit -d --with-coverage --cover-package=st_engine --cover-html --traverse-namespace --cover-inclusive --exe
+nosetests --with-xunit -d --with-coverage --cover-package=st_engine --cover-html --traverse-namespace --cover-inclusive
