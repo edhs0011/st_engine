e1d9bb71c1fe34eb1cc6136e12d84338fa2d96e6
Eddie S Hsieh <eddie_s_hsieh@trend.com.tw>
2017-02-18 09:59:29 +0800

    modify the unit test

--- unit_run.sh
+++ unit_run.sh
@@ -1,1 +1,1 @@
-nosetests --with-xunit -d --with-coverage --cover-package=st_engine --cover-html --traverse-namespace --cover-inclusive
+nosetests --with-xunit -d --with-coverage --cover-package=src --cover-html --traverse-namespace --cover-inclusive
